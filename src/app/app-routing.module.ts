b116b600652936ee2fe46e06e627a9a3ebdc6ca2
LadenxxxxD_msi <1344299971@qq.com>
2020-06-21 04:02:17 +0800

    env init 01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DetailsComponent } from './details/details.component';
+import { LibraryComponent } from './library/library.component';
 
 const routes: Routes = [
 
-{
-  path:'',
-  redirectTo:'login',
-  pathMatch:'full'
-},
-{
-  path: 'login',
-  component: LoginComponent
-},
-{
-  path:'details',
-  component: DetailsComponent
-}
+  {
+    path: '',
+    redirectTo: 'library',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
+  },
+  {
+    path: 'library',
+    component: LibraryComponent
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { BootstrapModalModule } from 'ng2-bootstrap-modal';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,12 +14,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { DetailsComponent } from './details/details.component';
+import { LibraryComponent } from './library/library.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DetailsComponent
+    DetailsComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +29,7 @@ import { DetailsComponent } from './details/details.component';
     HttpClientModule,
     AppRoutingModule,
     BootstrapModalModule,
+    NgZorroAntdModule,
     ModalModule.forRoot()
 
   ],
